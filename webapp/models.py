64ed4df484738ae1f18873131b1aff98b9b61fde
464716 <diesel.kg@mail.ru>
2020-07-08 22:54:19 +0600

    переименоввал Подкатегорию в Родительскую категорию, сделал миграции

--- webapp/models.py
+++ webapp/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     title = models.CharField(max_length=50, verbose_name='Категория')
     subcategory = models.ForeignKey("Category", on_delete=models.PROTECT, null=True,
-                                    blank=True, verbose_name="Подкатегории")
+                                    blank=True, verbose_name="Родительская категория")
 
     def __str__(self):
         return self.title
