31c73d98fd9908cf096f82facac62388fb04cc1d
464716 <diesel.kg@mail.ru>
2020-07-09 18:59:03 +0600

    убрал кнопку выввода свежих новостей, сделал это автоматиеским выводом новостей по свежести

--- webapp/views.py
+++ webapp/views.py
@@ -15,15 +15,12 @@ class IndexView(ListView):
     template_name = 'index.html'
     model = Article
     context_object_name = "articles"
-    # ordering = '-date'
+    ordering = ['-date']
     paginate_by = 3
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data()
         views = self.request.GET.get('views')
-        date = self.request.GET.get('date')
-        if date:
-            context['articles'] = Article.objects.order_by('-date')
         if views:
             context['articles'] = Article.objects.order_by('-views')
         return context
